2402d0e0982ac07696deebcd0af9d9d8c69d70ba
mikecocklin <mike.cocklin@making-it-simple.com>
2017-05-10 11:15:47 +0100

    Some styling and nav bar changes
    Change of nav bar links and styling

--- resources/views/cars/list.blade.php
+++ resources/views/cars/list.blade.php
@@ -3,35 +3,48 @@
 @section('title', 'Car List')
 
 @section('content')
-@if(Session::has('message'))
-    <div class="alert alert-success"><span class="glyphicon glyphicon-ok"></span><em> {!! session('message') !!}</em></div>
-@endif
-<table class="table table-striped table-bordered">
-<thead>
-		<tr>
-			<td>Make</td>
-			<td>Actions</td>
-			<td>Delete</td>
-		</tr>
-	</thead>
-<tbody>
 
-@foreach ($cars as $car)
-<tr>
-<td class="td">{{ $car->make }}</td>
-<td class="td">
-	<a class="btn btn-small btn-success" href="{{ URL::to('cars/' . $car->id) }}">Show this Car</a>
-	<a class="btn btn-small btn-success" href="{{ URL::to('cars/' . $car->id . '/edit') }}">Edit this Car</a>
-</td>
-<td>
-	{{ Form::open(array('url' => 'cars/' . $car->id, 'class' => 'pull-left')) }}
-        {{ Form::hidden('_method', 'DELETE') }}
-        {{ Form::submit('Delete this car', array('class' => 'btn btn-warning')) }}
-    {{ Form::close() }}
-</td>
-</tr>
-@endforeach
-</tbody>
-</table>
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-success">
+
+                @if(Session::has('message'))
+				    <div class="alert alert-success"><span class="glyphicon glyphicon-ok"></span><em> {!! session('message') !!}</em></div>
+				@endif
+				@if(Auth::check())
+               <div class="panel-heading">List of Cars - <a href="{{ URL::to('cars/create') }}">Create New Car</a></div>
+                                
+                      <!-- Table -->
+
+                      <table class="table">
+                          <tr>
+                              <th>Make of Car</th>
+                              <th>Actions</th>
+                              <th>Delete</th>
+                          </tr>
+							@foreach ($cars as $car)
+							<tr>
+							<td>{{ $car->make }}</td>
+							<td>
+								<a class="btn btn-small btn-success" href="{{ URL::to('cars/' . $car->id) }}">Show this Car</a>
+								<a class="btn btn-small btn-success" href="{{ URL::to('cars/' . $car->id . '/edit') }}">Edit this Car</a>
+							</td>
+							<td>
+								{{ Form::open(array('url' => 'cars/' . $car->id, 'class' => 'pull-left')) }}
+							        {{ Form::hidden('_method', 'DELETE') }}
+							        {{ Form::submit('Delete this car', array('class' => 'btn btn-warning')) }}
+							    {{ Form::close() }}
+							</td>
+							</tr>
+							@endforeach
+                      </table>
+                    @endif
+
+    </div>
+            @if(Auth::guest())
+              <a href="/login" class="btn btn-info"> You need to login >></a>
+            @endif
+        </div>
+    </div>
 
 @endsection
--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,12 +1,60 @@
+    <div id="app">
+        <nav class="navbar navbar-default navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
 
-<nav class="navbar navbar-inverse">
-	<div class="navbar-header">
-		<a class="navbar-brand" href="{{ url('home') }}">Home</a>
-	</div>
-	<ul class="nav nav-tabs">
-		<li><a href="{{ URL::to('cars') }}">View All Cars</a></li>
-		@if( ! empty($editpage))
-  			<li><a href="{{ URL::to('cars/create') }}">Create New Car</a></li>
-		@endif
-	</ul>
-</nav>
+                    <!-- Collapsed Hamburger -->
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                        <span class="sr-only">Toggle Navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+                    <!-- Branding Image -->
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
+                </div>
+
+                <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="nav navbar-nav">
+                        &nbsp;
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="nav navbar-nav navbar-right">
+                        <!-- Authentication Links -->
+                        @if (Auth::guest())
+                           
+                            <li><a href="{{ route('login') }}">Login</a></li>
+                            <li><a href="{{ route('register') }}">Register</a></li>
+                        @else
+                             <li><a href="{{ url('/') }}">See GoT Characters</a></li>
+                             <li><a href="{{ URL::to('cars') }}">See Cars</a></li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu" role="menu">
+                                    <li>
+                                        <a href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            Logout
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                        @endif
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
