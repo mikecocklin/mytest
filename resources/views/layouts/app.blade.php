3237b3c683b0924fb707c464d5b625b1e74b8278
mikecocklin <mike.cocklin@making-it-simple.com>
2017-05-10 09:47:11 +0100

    Slight tweeks
    Added new links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,9 +50,12 @@
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @if (Auth::guest())
+                           
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
+                             <li><a href="{{ url('/') }}">See GoT Characters</a></li>
+                             <li><a href="{{ URL::to('cars') }}">See Cars</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
